3195999619429753c47ae85a63992ab2e9d097fa
Arenagos <dfarenas7q@misena.edu.co>
2023-08-21 18:34:19 -0500

    diegoajuste

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import express from "express";
-import {dirname} from 'path';
-import { fileURLToPath } from "url";
-
-const app = express();
-
-dirname(fileURLToPath(import.meta.url))
-
-app.set('views', join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res)=> res.render('index'))
-
-app.listen(3000)
+console.long('hello world')
