e2e49395477e188e2a4468f10336abb9bd9622e4
Arenagos <dfarenas7q@misena.edu.co>
2023-08-21 23:24:55 -0500

    diegoajustess

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import express from 'express'
 import {dirname, join} from 'path'
 import { fileURLToPath } from 'url'
 
+import indexRoutes from './routes/index.js'
+
 
 const app = express()
 
@@ -10,8 +12,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.set('views', join( __dirname,'views'))
 app.set('view engine', 'ejs')
+app.use(indexRoutes)
 
-app.get('/', (req, res) => res.render('index.ejs'))
 
 app.listen(3000)
 console.log('server is listenig on port', 3000)
