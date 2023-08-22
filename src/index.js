ae23dfdc843ba9c497b284b0f9270bbfba68bbb8
Arenagos <dfarenas7q@misena.edu.co>
2023-08-21 20:53:10 -0500

    diegoajustes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.long('hello world')
+
+import express from 'express'
+const app = express()
+app.get('/', (req, res) => res.send('Trueke.com'))
+
+app.listen(3000)
+console.log('server is listenig on port', 3000)
