ef7476271ab587c8dcab7af96e23f2200cfd15fa
Arenagos <dfarenas7q@misena.edu.co>
2023-08-22 16:32:40 -0500

    diegoindex.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,7 @@ import{ Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => res.render('index',{ title: 'first wed with node'}))
-
 router.get('/about', (req, res) => res.render('about',{title: 'about me'}))
-
 router.get('/contact', (req, res) => res.render('contact',{title: 'contact page'}))
 
 export default router
